c711cc404b5264becd5d4472acc14d29d8c6f602
arif_meilanov <arif.m.04@mail.ru>
2024-12-24 14:03:10 +0300

    Backend files

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
