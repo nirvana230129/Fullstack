e36d2f74398c9e162c50ca6c8ef018c543ec393e
arif_meilanov <arif.m.04@mail.ru>
2024-12-24 15:16:35 +0300

    Frontend files

--- frontend/app.py
+++ frontend/app.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from flask import Flask, jsonify
+import requests
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Hello from Flask!"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route('/api/data')
+def get_data():
+    response = requests.get('http://backend:8000/api/data')
+    return jsonify(response.json())
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
